82d916b5509eab69834f9cc859fb21222fb631e9
PloyChan <ploychompoo@geniustree.co.th>
2023-04-02 00:04:58 +0700

    project helloClient

--- src/main/java/org/example/HelloClient.java
+++ src/main/java/org/example/HelloClient.java
@@ -4,12 +4,18 @@ import java.util.Optional;
 import java.util.ServiceLoader;
 
 public class HelloClient {
-    public static void main( String[] args )
-    {
+
+    public static HelloProvider getDefault(){
         ServiceLoader<HelloProvider> helloProviderLoader = ServiceLoader.load(HelloProvider.class);
-        Optional<HelloProvider> findFirst = helloProviderLoader.findFirst();
-        HelloProvider helloProvider = findFirst.orElseThrow();
-        helloProvider.hello();
+
+        for (HelloProvider provider: helloProviderLoader){
+            return provider;
+        }
+        throw new Error("Something wrong");
     }
 
+    public static void main(String[] args) {
+        HelloProvider helloProvider = HelloClient.getDefault();
+        helloProvider.hello();
+    }
 }
